5347290966dcaeede1fbbd00657d4c7eea816d7f
Isabela Neves <93232499+bela-isa@users.noreply.github.com>
2025-04-30 17:06:51 -0300

    atualizando  API_URL com s.tsecrets.get

--- frontend/pages/2_DIRETRIZES.py
+++ frontend/pages/2_DIRETRIZES.py
@@ -6,7 +6,8 @@ from pathlib import Path
 from datetime import datetime
 import time
 
-API_URL = os.getenv("API_URL", "http://localhost:8000")
+#API_URL = os.getenv("API_URL", "http://localhost:8000")
+API_URL = st.secrets.get("API_URL", "http://localhost:8000")
 
 # Configuração da página
 st.set_page_config(
